62f935533b66fb0eb8689b1aa11667b4a7897068
prakashvelusamy-hcl <prakashvelusamy1999@gmail.com>
2025-06-13 13:16:28 +0530

    usecase-9

--- terraform.tfvars
+++ terraform.tfvars
@@ -1,7 +1,5 @@
 vpc_cidr         = "10.10.0.0/16"
-environment       = "dev"
+# environment       = "dev"
 pub_sub_count    = 2
 priv_sub_count   = 2
 nat_count        = 1
-patient_repo_uri = "495599733393.dkr.ecr.ap-south-1.amazonaws.com/patient"
-appointment_repo_uri = "495599733393.dkr.ecr.ap-south-1.amazonaws.com/appointment"
--- variables.tf
+++ variables.tf
@@ -2,10 +2,10 @@ variable "vpc_cidr" {
   description = "CIDR block for the VPC"
   type        = string
 }
-variable "environment" {
-  description = "The Environment we are using"
-  type = string
-}
+# variable "environment" {
+#   description = "The Environment we are using"
+#   type = string
+# }
 variable "pub_sub_count" {
   description = "Number of public subnets"
   type        = number
@@ -19,13 +19,4 @@ variable "priv_sub_count" {
 variable "nat_count" {
   description = "Number of NAT gateways"
   type        = number
-}
-
-variable "patient_repo_uri" {
-    description = "The Patient image repo URL"
-    type = string
-}
-variable "appointment_repo_uri" {
-description = "The appointment image repo URL"
-type = string
 }
