455088b989f64536f51ca8ce8dfb41fb4d315298
Prakash_V <96634210+prakashvelusamy-hcl@users.noreply.github.com>
2025-06-16 09:12:34 +0530

    fix

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket       = "aws-state-s3"
     key          = "hcl-usecase-9/terraform.tfstate"
-    profile      = "devops"
+    #profile      = "devops"
     region       = "ap-south-1"
     encrypt      = true
     use_lockfile = true
--- providers.tf
+++ providers.tf
@@ -22,5 +22,5 @@ terraform {
 }
 provider "aws" {
   region  = var.region
-  profile = "devops"
+  #profile = "devops"
 }
